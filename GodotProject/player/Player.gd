f3db837674047296bd84a84f25d7ab973c05e421
jpt <joaopedroturri@gmail.com>
2018-08-22 15:53:18 -0300

    Fixed rotation bug

--- GodotProject/player/Player.gd
+++ GodotProject/player/Player.gd
@@ -3,18 +3,14 @@ extends KinematicBody2D
 var speed = 300
 var direction = Vector2(0,0)
 var angle = 0
-<<<<<<< HEAD
-var spiderAngle = 0
 var fall = false
 var velocity = Vector2(0, 250)
 var gravity = 300
-=======
 var front = 0
 var back = 180
 var step = 1
 var rotationSpeed = 15
 var mustRotate = false 
->>>>>>> 1de7ade282b373667694756a5b1ef71faef8849d
 
 func _physics_process(delta):
 	direction = Vector2(0,0)
@@ -24,6 +20,7 @@ func _physics_process(delta):
 		getRotation()
 		mustRotate = false
 	evalRotation()
+
 		
 	if front < (angle-5) or front > (angle+5):
 		$SpiderSprite.rotation_degrees += (step * rotationSpeed)
@@ -31,15 +28,10 @@ func _physics_process(delta):
 		$SpiderSprite.rotation_degrees = angle
 	move_and_slide(direction*speed)
 	
-<<<<<<< HEAD
 	if fall:
 		velocity.y += gravity * delta
 		velocity = move_and_slide(velocity, Vector2(0, -1))
-	
-=======
-	
 		
->>>>>>> 1de7ade282b373667694756a5b1ef71faef8849d
 func update_direction():
 	if Input.is_action_pressed("ui_up"):
 		direction.y -= 1
@@ -56,9 +48,6 @@ func update_direction():
 	if Input.is_action_pressed("ui_right"):
 		direction.x += 1
 		mustRotate = true
-
-
-#GRAUS
 
 func getRotation():
 	if direction.x == 0:
@@ -94,25 +83,12 @@ func evalRotation():
 			step = 1
 		else:
 			step = -1
-			#angle = (360 - angle)%360 * -1
 	else:
 		if angle >= back and angle < front:
 			step = -1
-			#angle = (360 - angle)%360 * -1
 		else:
 			step = 1
 	
-<<<<<<< HEAD
-	if  spiderAngle != angle:
-		var fator = 1
-		if angle - spiderAngle < 0 or (angle >= 225 and spiderAngle <= 90):
-			fator = -1
-			if (0 <= angle and angle < 90) and (spiderAngle > 180):
-				fator = 1
-		$SpiderSprite.rotation_degrees += 5*fator
-=======
 	if $SpiderSprite.rotation_degrees == 0 and step == -1:
 		$SpiderSprite.rotation_degrees = 360
-
 		
->>>>>>> 1de7ade282b373667694756a5b1ef71faef8849d
