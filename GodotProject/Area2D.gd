ef5b7ddef2a60a332cc44020f026c7fd1dd2491a
JP <joaopedroturri@gmail.com>
2018-10-10 12:37:21 -0300

    Changed spider to RigidBody2d. Basic movement works

--- GodotProject/Area2D.gd
+++ GodotProject/Area2D.gd
@@ -8,46 +8,34 @@ var index = 0
 
 func _physics_process(delta):
 	
-	var array = [0, 0, 0, 0, 0]
-	var index = 0
-	for x in get_overlapping_areas():
-		if x.name == "DetectBranch":
-			array[index] = 5
-		if x.name == "FrontArea":
-			array[index] = 1
-		if x.name == "RightArea":
-			array[index] = 2
-		if x.name == "LeftArea":
-			array[index] = 3
-		if x.name == "BackArea":
-			array[index] = 4
-		index += 1
-	if 1 in array:
-		$"/root/Root/Spider".fora = false
-	else:
-		$"/root/Root/Spider".fora = true
-		
-	if 2 in array:
-		$"/root/Root/Spider".foraRight = false
-	else:
-		$"/root/Root/Spider".foraRight = true
-		
-	if 3 in array:
-		$"/root/Root/Spider".foraLeft = false
-	else:
-		$"/root/Root/Spider".foraLeft = true
-		
-	if 4 in array:
-		$"/root/Root/Spider".foraBack = false
-	else:
-		$"/root/Root/Spider".foraBack = true
-		
-	if 5 in array:
+	if get_overlapping_areas():
 		spiderInArea = true
+		$"/root/Root/Spider2".gravity_scale = 0
 		if init: #Semi-gambiarra. Precisa inicializar como falso
-			$"/root/Root/Spider".fall = false
+			$"/root/Root/Spider2".fall = false
 			init = false
 	else:
 		spiderInArea = false
-		$"/root/Root/Spider".fall = true
-		$"/root/Root/Spider".direction = Vector2(0, 1)
+		$"/root/Root/Spider2".fall = true
+		#$"/root/Root/Spider2".direction = Vector2(0, 1)
+		$"/root/Root/Spider2".gravity_scale = 10
+	"""
+
+	for x in get_overlapping_areas():
+	
+		if x.name == "DetectBranch":
+
+
+		
+
+			spiderInArea = true
+			$"/root/Root/Spider2".gravity_scale = 0
+			if init: #Semi-gambiarra. Precisa inicializar como falso
+				$"/root/Root/Spider2".fall = false
+				init = false
+		else:
+			spiderInArea = false
+			$"/root/Root/Spider2".fall = true
+			#$"/root/Root/Spider2".direction = Vector2(0, 1)
+			$"/root/Root/Spider2".gravity_scale = 5
+	"""
---
+++ GodotProject/player/Player_rigido.gd
@@ -0,0 +1,185 @@
+extends RigidBody2D
+
+var speed = 300
+var direction = Vector2(0,-1)
+var angle = 0
+var fall = false
+var velocity = Vector2(0, 250)
+var gravity = 600
+var front = 0
+var back = 180
+var step = 1
+var rotationSpeed = 15
+var mustRotate = true 
+var isRotating = false
+var web_resource = 0
+var web_parent = null
+var web = null
+var bunda_position = Vector2(800, 600-45)
+var dirKeys = [0, 0, 0, 0]
+
+func _physics_process(delta):
+	if not 1 in dirKeys:
+		set_linear_velocity(Vector2(0,0))
+
+	direction = Vector2(0,0)
+	
+	update_direction()
+	getRotation()
+	
+	if direction != Vector2(0,0):
+		bunda_position.x = self.position.x + direction.x*-50
+		bunda_position.y = self.position.y + direction.y*-50
+		
+	if mustRotate:
+		getRotation()
+		mustRotate = false
+	evalRotation()
+
+	#Esse bloco elimina ambiguidades, pois a direcao Cima pode ser
+	#tanto 180 como -180
+	if angle == -179:
+		if front < -165 or front > 170:
+			self.rotation_degrees = -179
+			
+	#Rotaciona a aranha	
+	if front < (angle-5) or front > (angle+5):
+		isRotating = true
+		self.rotation_degrees += (step * rotationSpeed)
+		$AnimatedSprite.playing = true
+	else:
+		self.rotation_degrees = angle
+		isRotating = false
+		
+	if 1 in dirKeys and not fall:
+		#Verifica se alguma das teclas direcionais está apertada e processa o movimento
+		set_linear_velocity(direction.normalized()*speed)
+	
+	"""
+	if fall:
+		direction.x = 0
+		direction.y = 1
+		velocity.y = gravity
+		set_linear_velocity(direction.normalized()*velocity)
+	"""
+		
+func update_direction():
+	if Input.is_action_pressed("ui_up") and not fall:
+		direction.y = -1
+		dirKeys[0] = 1
+		if not dirKeys[2] and not dirKeys[3]:
+			direction.x = 0
+		$AnimatedSprite.playing = true
+		
+	if Input.is_action_pressed("ui_down") and not fall:
+		direction.y = 1
+		dirKeys[1] = 1
+		if not dirKeys[2] and not dirKeys[3]:
+			direction.x = 0
+		$AnimatedSprite.playing = true
+	
+	if Input.is_action_pressed("ui_left") and not fall:
+		direction.x = -1
+		dirKeys[2] = 1
+		if not dirKeys[0] and not dirKeys[1]:
+			direction.y = 0
+		$AnimatedSprite.playing = true
+	
+	if Input.is_action_pressed("ui_right") and not fall:
+		direction.x = 1
+		dirKeys[3] = 1
+		if not dirKeys[0] and not dirKeys[1]:
+			direction.y = 0
+		$AnimatedSprite.playing = true
+		
+	if Input.is_action_just_released("ui_up"):
+		dirKeys[0] = 0
+
+	if Input.is_action_just_released("ui_down"):
+		dirKeys[1] = 0
+
+	if Input.is_action_just_released("ui_left"):
+		dirKeys[2] = 0
+
+	if Input.is_action_just_released("ui_right"):
+		dirKeys[3] = 0
+		
+	if !Input.is_action_pressed("ui_up") and !Input.is_action_pressed("ui_right") and !Input.is_action_pressed("ui_left") and !Input.is_action_pressed("ui_down") and !isRotating and !fall:
+		$AnimatedSprite.playing = false
+		
+	if Input.is_action_just_pressed("ui_accept") and not $StickTimer.time_left:
+		if fall:
+			if $"/root/Root/BackgroundSprite/Area2D".spiderInArea == true:
+				fall = false
+				direction.x = 0
+				direction.y = 1
+				angle = 0
+				$AnimatedSprite.playing = false
+				$StickTimer.start()
+		else:
+			fall = true
+			$StickTimer.start()
+	
+#	if Input.is_action_pressed("secretar"):
+#		grudando = true
+	
+func getRotation():
+	if direction.x == 0:
+		if direction.y == -1:
+			#angle = 180
+			angle = -179
+		if direction.y == 1:
+			angle = 0
+
+	if direction.x == 1:
+		if direction.y == -1:
+			#angle = 225
+			angle = -135
+		if direction.y == 0:
+			#angle = 270
+			angle = -90
+		if direction.y == 1:
+			#angle = 315
+			angle = -45
+			
+
+	if direction.x == -1:
+		if direction.y == -1:
+			angle = 135
+		if direction.y == 0:
+			angle = 90
+		if direction.y == 1:
+			angle = 45
+
+func evalRotation():
+	front = int(self.rotation_degrees)
+	
+	var fator
+	if front >= 0:
+		fator = -1
+	else:
+		fator = 1
+	back = front+(180 * fator)
+
+	if front >= 0:
+		if angle > 0:
+			if angle < front:
+				step = -1
+			else:
+				step = 1
+		else:
+			if angle < back:
+				step = 1
+			else:
+				step = -1
+	else:
+		if angle > 0:
+			if angle > back:
+				step = -1
+			else:
+				step = 1
+		else:
+			if angle < front:
+				step = -1
+			else:
+				step = 1
