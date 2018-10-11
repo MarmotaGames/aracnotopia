481ccd914dfd17856dfb21e55fbd4d661c054c15
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2018-10-11 00:21:45 -0300

    Changed the SpiderDummy to the real Spider

--- GodotProject/web/ball/Ball.gd
+++ GodotProject/web/ball/Ball.gd
@@ -5,8 +5,7 @@ var over = false
 func _process(delta):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and over:
 		var mousePos = get_viewport().get_mouse_position()
-		position += mousePos - global_position*0.85
-#		position += mousePos
+		position += mousePos - global_position
 
 func _on_Control_mouse_entered():
 	over = true
